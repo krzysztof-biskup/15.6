6b420e5b5aaa391d57d2d6b2165d86f074aa22cd
Krzysiek <krzysiek@popracy.eu>
2019-10-16 20:07:18 +0200

    Ready for review

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
-function Button() {
-    
+function Button(text) {
+    this.text = text || 'Hello';
 }
+
+Button.prototype = {
+    create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+    }
+};
+
+var btn1 = new Button('Hello!');
+
+btn1.create();
